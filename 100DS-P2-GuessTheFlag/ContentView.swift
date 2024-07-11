b448d52f883b4cc4dc88d6270f94632e2e261e61
Erica Sampson <ericabcherry@gmail.com>
2024-07-11 15:40:32 -0400

    Initial Commit

---
+++ 100DS-P2-GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100DS-P2-GuessTheFlag
+//
+//  Created by Erica Sampson on 2024-07-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DS-P2-GuessTheFlag/_00DS_P2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DS_P2_GuessTheFlagApp.swift
+//  100DS-P2-GuessTheFlag
+//
+//  Created by Erica Sampson on 2024-07-11.
+//
+
+import SwiftUI
+
+@main
+struct _00DS_P2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
